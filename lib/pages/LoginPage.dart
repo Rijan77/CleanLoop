8349bbc124a39ee6fa56b43d09a7042c6c6972c5
Rijan77 <rijanacharya73@gmail.com>
2024-11-22 21:45:20 +0545

    Initial design of login page

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -11,6 +11,16 @@ class _LoginpageState extends State<Loginpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body:
+      Column(
+        children: [
+          Image.asset("lib/assets/images/Vector 2.png"),
+
+          SizedBox(height: 5,),
+
+          Image.asset("lib/assets/images/loginpage.icon.png", width: 300,)
+        ],
+      ),
 
     );
   }
