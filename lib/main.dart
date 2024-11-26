d1d3fdf896788740dd90c8759c12a0d33394d7f0
Rijan77 <rijanacharya73@gmail.com>
2024-11-26 22:05:48 +0545

    Resolve merge conflict
    # Conflicts:
    #	lib/main.dart
    #	lib/pages/LoginPage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cleanloop/pages/SplasScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -11,6 +12,7 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Splashscreen(),
+      home: const Splashscreen(),
     );
   }
 }
