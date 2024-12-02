db08014469f06b096f799bd416a2363e3835094a
Rijan77 <rijanacharya73@gmail.com>
2024-12-02 09:21:43 +0545

    Resolved conflicts after applying stash

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:cleanloop/pages/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -17,7 +16,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
